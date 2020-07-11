6a75157d6e84c46281bd2be7e433d8cfa95aca63
eslam <emohamef733@gmail.com>
2020-07-11 22:19:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const fs = require('fs');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/blog-nestjs'));
+app.use(express.static(__dirname + '/dist/frontend-posts-app'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/blog-nestjs/index.html'));
+res.sendFile(path.join(__dirname+'/dist/frontend-posts-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
